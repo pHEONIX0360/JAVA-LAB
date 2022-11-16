2824a2bcf540b6cc50be1d90b1cd531011afd199
Joeson Stanes <95993837+pHEONIX0360@users.noreply.github.com>
2022-11-16 14:54:30 +0530

    Create palindrom.java

---
+++ palindrom.java
@@ -0,0 +1,29 @@
+public class Palindrome {
+	public static void main(String[] args)
+	{
+		Scanner in = new Scanner(System.in);
+		StringBuffer str1;
+		StringBuffer rev;
+		int n,length;
+		System.out.println("Enter the string");
+		str1= new StringBuffer();
+		str1=str1.append(in.nextLine());
+		rev=new StringBuffer();
+		length=str1.length();
+		for(int i=0;i<length ;i++)
+			
+		{
+ 			rev =rev.append(str1.charAt(i));
+ 			
+		}System.out.println(""+rev);
+		if(rev==str1)
+		{
+System.out.println("Entered string is palindrome");
+		}
+		else
+		{
+			System.out.println("Entered string is  not palindrom");
+		}
+	}
+
+}
