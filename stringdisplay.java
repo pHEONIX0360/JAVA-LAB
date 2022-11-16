04898a193b2f32cc4cdde0a7e889187837aa499e
Joeson Stanes <95993837+pHEONIX0360@users.noreply.github.com>
2022-11-16 14:52:01 +0530

    Create stringdisplay.java

---
+++ stringdisplay.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Stringdisplay {
+
+	public static void main(String[] args) {
+Scanner in = new Scanner(System.in);
+String  str1;
+System.out.println("Enter the string");
+
+str1 = new String();
+str1=in.nextLine();
+System.out.println("The entered string is :"+str1);
+	}
+
+}
